653e43875b63dcd88da08b4c49cfe927e78e8c47
jsubuyuj <jsubuyuj-2016243@kinal.edu.gt>
2021-10-26 19:41:52 -0600

    Ver libros en pagina de prestamo

--- Biblioteca243/src/app/components/prestamo/prestamo.component.ts
+++ Biblioteca243/src/app/components/prestamo/prestamo.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book.model'; 
+import { RestBookService } from 'src/app/services/restBook/rest-book.service';
 
 @Component({
   selector: 'app-prestamo',
   templateUrl: './prestamo.component.html',
-  styleUrls: ['./prestamo.component.scss']
+  styleUrls: ['./prestamo.component.scss'],
+  providers: [ RestBookService ]
 })
 export class PrestamoComponent implements OnInit {
+  Book:Book
+  libros;
 
-  constructor() { }
+  constructor(public bookService: RestBookService) {
+    this.Book = new  Book('','','','','','','','',null,null,'','')
+   }
 
   ngOnInit(): void {
+    this.showBook();
+  }
+
+  showBook(){
+    this.bookService.showBook().subscribe(
+      response =>{
+        console.log(response);
+        this.libros = response;
+        console.log(this.Book)
+      }
+    )
   }
 
 }
